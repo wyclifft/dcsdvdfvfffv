9f39e70f6b34175964b8213a726d4e744e1ded3a
wyclifft <wycliffmutethia8@gmail.com>
2025-10-05 13:41:49 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+      },
+    },
+  },
+  publicDir: 'public',
 });
